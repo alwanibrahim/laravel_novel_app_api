c082c223ead7f332eef1ba6bd51ec9f3a4d3d9d8
alwanibrahim <alwanibrahim142@gmail.com>
2025-05-07 13:29:46 +0700

    change Author Resource

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Filament\Facades\Filament;
+
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Filament::serving(function () {
+    Filament::registerNavigationGroups([
+        'Main',
+    ]);
 });
